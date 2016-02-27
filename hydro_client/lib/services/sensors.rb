000c05ed72a7eaebeb6af5edadd437cb81dbe0b6
Juan Pablo Bochard <jbochard@gmail.com>
2016-02-27 01:16:22 -0300

    Mejora

--- hydro_client/lib/services/sensors.rb
+++ hydro_client/lib/services/sensors.rb
@@ -23,15 +23,23 @@ require 'serialport'
         cmdObj = Environment.sensors["read"].select { |cmd| cmd["sensor"].upcase == command.upcase }.first
         if ! cmdObj.nil?
             @semaphore.synchronize {
-                @serial.write("#{cmdObj['command'].upcase}\n")
-                @serial.flush            
-                line = readLine
-                return { :state => 'ERROR', :value => 'I/O ERROR' } if line.nil?
+                begin
+                    @serial.write("#{cmdObj['command'].upcase}\n")
+                    @serial.flush            
+                    line = readLine
+                    return { :state => 'ERROR', :value => 'READ ERROR' } if line.nil?
 
-                res = line.scan(/#{cmdObj['command'].upcase}\s+([^ ]+)\s*([^ ]*)$/).last
-                state = res[0]
-                value = res[1] if res.length > 1
-                return { :state => state, :value => value }
+                    res = line.scan(/#{cmdObj['command'].upcase}\s+([^ ]+)\s*([^ ]*)$/).last
+                    state = res[0]
+                    value = res[1] if res.length > 1
+                    return { :state => state, :value => value }
+                rescue Excpetion => e
+                    puts "Restore connection: #{e}"
+                    puts e.backtrace
+
+                    @serial.close
+                    @serial = SerialPort.new(Environment.config["serial"]["serial_port"], Environment.config["serial"]["baud_rate"], Environment.config["serial"]["data_bits"], Environment.config["serial"]["stop_bits"], SerialPort::NONE)
+                end                
             }
         end
         { :state => "ERROR", :value => "#{command} NOT FOUND" }
@@ -41,19 +49,27 @@ require 'serialport'
         cmdObj = Environment.sensors["write"].select { |cmd| cmd["switch"].upcase == relay.upcase }.first
         if ! cmdObj.nil?
             @semaphore.synchronize {
-                command = cmdObj["on"].upcase if state.upcase == 'ON'
-                command = cmdObj["off"].upcase if state.upcase == 'OFF'
+                begin
+                    command = cmdObj["on"].upcase if state.upcase == 'ON'
+                    command = cmdObj["off"].upcase if state.upcase == 'OFF'
 
-                @serial.write("#{command}\n")
-                @serial.flush
-                
-                line = readLine
-                return { :state => 'ERROR', :value => 'I/O ERROR' } if line.nil?
+                    @serial.write("#{command}\n")
+                    @serial.flush
+                    
+                    line = readLine
+                    return { :state => 'ERROR', :value => 'READ ERROR' } if line.nil?
 
-                res = line.scan(/#{command}\s+([^ ]+)\s*([^ ]*)$/).last
-                state = res[0]
-                value = res[1] if res.length > 1
-                return { :state => state, :value => value }
+                    res = line.scan(/#{command}\s+([^ ]+)\s*([^ ]*)$/).last
+                    state = res[0]
+                    value = res[1] if res.length > 1
+                    return { :state => state, :value => value }
+                rescue Excpetion => e
+                    puts "Restore connection: #{e}"
+                    puts e.backtrace
+
+                    @serial.close
+                    @serial = SerialPort.new(Environment.config["serial"]["serial_port"], Environment.config["serial"]["baud_rate"], Environment.config["serial"]["data_bits"], Environment.config["serial"]["stop_bits"], SerialPort::NONE)
+                end
             }
         end
         { :state => "ERROR", :value => "#{relay}-#{state} NOT FOUND" }
